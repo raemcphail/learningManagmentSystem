565b114b03fe53d6f03a46674d1ddf2da5f92173
Louis Johnson <louisj381@gmail.com>
2018-04-01 20:10:06 -0600

    configured dividers properly

--- Learning Management System/src/frontEnd/DashboardFrame.java
+++ Learning Management System/src/frontEnd/DashboardFrame.java
@@ -1,8 +1,6 @@
 package frontEnd;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
+import javax.swing.*;
 public class DashboardFrame extends JFrame {
 	/**
 	 * the overlaying panel
@@ -17,15 +15,46 @@ public class DashboardFrame extends JFrame {
 	 * the lower portion of the frame, which is an instance of contentPanel, and varies based on user input
 	 */
 	JPanel lowerPanel;
+	/**
+	 * the title Panel to be added to the upperPanel
+	 */
+	TitlePanel title;
+	/**
+	 * the course, middle bar to be added to the upperPanel
+	 */
+	CoursePanel middleBar;
+	/**
+	 * the divider between the middlebar and the content pane
+	 */
+	JSplitPane middle;
+	/**
+	 * the divider between the title and the courseBar
+	 */
+	JSplitPane upperMiddle;
 	
 	public DashboardFrame() {
 		setSize(700,700);
 		mainPanel = new JPanel();
 		this.setContentPane(mainPanel);
-		TitlePanel title = new TitlePanel();
-		mainPanel.add(title);
-		CoursePanel middleBar = new CoursePanel();
-		mainPanel.add(middleBar);
-		//this.pack();
+		
+		upperPanel = new JPanel();
+		lowerPanel = new JPanel();
+		
+		title = new TitlePanel();
+		
+		middleBar = new CoursePanel();
+		
+		addDividers();
+		this.pack();
+	}
+	
+	public void addDividers()
+	{
+		upperMiddle = new JSplitPane(JSplitPane.VERTICAL_SPLIT, title, middleBar);
+		upperPanel.add(upperMiddle);
+		
+		middle = new JSplitPane(JSplitPane.VERTICAL_SPLIT, upperPanel, lowerPanel);
+		mainPanel.add(middle);
+
 	}
 }
